ed4e3ab1b70f001c59f9d8f4c1e64e4f1ba4d5d9
SHARP.CLAUDIA <2211143@fbisd.flourbluffschools.net>
2018-10-24 14:02:47 -0500

    image filter

---
+++ in_class_img.py
@@ -0,0 +1,20 @@
+from PIL import Image
+
+img = Image.open("beach.jpg")
+#img.show()#original image
+
+pixmap = img.load()
+
+r,g,b = pixmap[100,100]
+
+for i in range(img.size[0]):
+    for j in range(img.size[1]):#(0,img.size[1],6)   this limits the filter to every pixel, shows lines
+        #pixmap[i,j] = (255,255,255)
+        r,g,b = pixmap[i,j]
+        r += 255
+        g += 0
+        b += 0
+        pixmap[i,j] = (g,b,b)
+    
+img.show()
+img.save("beach_filter.jpg")
